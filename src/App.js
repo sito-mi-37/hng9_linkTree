62e5e5969a3fcd74fe41a2e1032991df2b946999
sito <sito.mi.37@gmail.com>
2022-11-03 14:09:31 +0100

    fix: included a netlify.toml file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
 import Layout from "./components/Layout";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Contact from "./pages/contact";
 
 function App() {
   return (
     <div className="App">
-      <Layout >
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
+      <Layout>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
       </Layout>
     </div>
   );
