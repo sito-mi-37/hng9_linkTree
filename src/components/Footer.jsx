290c18d5c769a50e17a2a93644d98571a04221f4
sito <sito.mi.37@gmail.com>
2022-11-03 11:26:11 +0100

    feat: Added routing to the different pages with react router dom

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+          <div className="zuri__logo">
+            Zuri<span>.</span>Internship
+          </div>
+          <div>HNG internship 9 Frontend Task</div>
+          <div>
+            <img src="I4G.png" alt="" />
+          </div>
+        </div>
+      </footer>
+  )
+}
+
+export default Footer
---
+++ src/pages/contact/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+        
+       
+
+    </div>
+  )
+}
+
+export default Contact
