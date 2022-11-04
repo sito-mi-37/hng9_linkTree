5d56eae32475d5361d8732c42c10181acc37ae2e
sito <sito.mi.37@gmail.com>
2022-11-04 17:43:06 +0100

    style: updated styles in footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
           <div className="zuri__logo">
             Zuri<span>.</span>Internship
           </div>
-          <div className='text-3xl text-red-600'>HNG internship 9 Frontend Task</div>
+          <div>HNG internship 9 Frontend Task</div>
           <div>
             <img src="I4G.png" alt="" />
           </div>
